9c4a93f818edc3431615d88b1aa6d9430db0a58a
mahael666 <mahael666@gmail.com>
2023-08-25 10:50:37 +0500

    Lesson_2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,9 @@
 
 public class Main {
     public static void main(String[] args) {
-        Print.printTreeWords();     // Задание №1
-        Print.printColor();         // Задание №2
-        Print.checkSumSing();       // Задание №3
-        Print.compareNumbers();     // Задание №4
+        Print.printTreeWords();     // Задание  №1
+        Print.printColor();         // Задание  №2
+        Print.checkSumSing();       // Задание  №3
+        Print.compareNumbers();     // Задание  №4
     }
 }
