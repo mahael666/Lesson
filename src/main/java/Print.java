1e0dd8ecb2ceaf8d72f1553680b4cb42ba8321bf
mahael666 <mahael666@gmail.com>
2023-08-25 10:44:44 +0500

    Lesson_2

---
+++ src/main/java/Print.java
@@ -0,0 +1,47 @@
+
+public class Print {
+
+    // Задание №1
+    public static void printTreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+    // Задание №2
+    public static void checkSumSing() {
+        int a = 23;
+        int b = -230;
+        int sum = a + b;
+        if (sum >= 0) {
+            System.out.println("Сумма положительня");
+        } else {
+            System.out.println("Сумма отрицательня");
+        }
+    }
+
+    // Задание №3
+    public static void printColor() {
+        int value = -99;
+        if (value <= 0) {
+            System.out.println("Красный");
+        }
+        if (0 < value && value <= 100) {
+            System.out.println("Желтый");
+        }
+        if (value > 100) {
+            System.out.println("Зеленый");
+        }
+    }
+
+    //Задание №4
+    public static void compareNumbers() {
+        int a = 23;
+        int b = 230;
+        if (a >= b) {
+            System.out.println("a>=b");
+        } else {
+            System.out.println("a<b");
+        }
+    }
+}
