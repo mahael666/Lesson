9a9ca5430553a987ecb4a36354edc3f2cf0c18e3
mahael666 <32843682+mahael666@users.noreply.github.com>
2023-09-21 20:30:35 +0600

    Lesson№8

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,9 @@
 
 import java.util.*;
-
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
 public class Main {
     public static void main(String[] arg) {
         ArrayList<String> arrayList = new ArrayList<>();
@@ -16,12 +19,38 @@ public class Main {
         arrayList.add("F");
         arrayList.add("G");
         Set<String> set = new LinkedHashSet<>(arrayList);
-        HashMap< String, Integer> hm = new HashMap<>();
-        for (Map.Entry<String, Integer> o : hm.entrySet()) {
-            System.out.println(o.getKey() + ": " + o.getValue());
-        }
         System.out.println(arrayList);
         System.out.println(set);
+
+        HashMap< String, Integer> hm = new HashMap<>();
+        for (String key : arrayList) {
+            var value = hm.get(key);
+
+            if (value != null) {
+                hm.put(key, value + 1);
+            } else {
+                hm.put(key, 1);
+            }
+        }
+        System.out.println(hm);
+
+        Phonebook phonebook = new Phonebook();
+
+        phonebook.add("Пудов", "89999999999");
+        phonebook.add("Сидоров", "89999999998");
+        phonebook.add("Пудов", "89999999997");
+        phonebook.add("Петров", "89999999996");
+        phonebook.add("Федоров", "89999999995");
+        phonebook.add("Пупков", "89999999994");
+        phonebook.add("Панкратов", "89999999993");
+        phonebook.add("Пудов", "89999999992");
+        phonebook.add("Сидоров", "89999999991");
+        phonebook.add("Петров", "89999999990");
+
+        System.out.println(phonebook);
+
+        System.out.println(phonebook.get("Иванов"));
+
     }
 
 
