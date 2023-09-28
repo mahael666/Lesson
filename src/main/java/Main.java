5383cfc2467f489f2b5b7cd6064417bb3515a7ed
mahael666 <32843682+mahael666@users.noreply.github.com>
2023-09-28 20:42:47 +0600

    Lesson№10

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,10 +2,27 @@ import java.util.stream.*;
 import java.util.*;
 import java.util.function.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        TrainStreamAPI.DemoStream();
-        StreamAPI.streamAPI();
-        FiveStreamApi.DemoStreamfive();
+
+        Apple apple1 = new Apple();
+        Apple apple2 = new Apple();
+        Apple apple3 = new Apple();
+        Apple apple4 = new Apple();
+
+        Orange orange1 = new Orange();
+        Orange orange2 = new Orange();
+
+        Box<Apple> box1 = new Box<Apple>(apple1, apple2, apple3, apple4);
+        Box<Orange> box2 = new Box<Orange>(orange1, orange2);
+
+        System.out.println(box1.compare(box2));
+
+        Box<Orange> box3 = new Box<Orange>();
+        box2.transfer(box3);
+
     }
 }
